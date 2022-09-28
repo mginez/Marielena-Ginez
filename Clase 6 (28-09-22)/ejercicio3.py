771c91c8dd74bbccfa9561f2920c89a735901618
mginez <mginez@correo.unimet.edu.ve>
2022-09-28 14:26:40 -0400

    🐍 Diccionarios

--- Clase 6 (28-09-22)/ejercicio3.py
+++ Clase 6 (28-09-22)/ejercicio3.py
@@ -3,13 +3,22 @@
 # El programa debe crear un diccionario con las palabras y sus traducciones. Después pedirá una frase 
 # en español y utilizará el diccionario para traducirla palabra a palabra. Si una palabra no está en 
 # el diccionario debe dejarla sin traducir.
-
 dictionary = {}
-translations = input('Please enter the dictionary ')
+translations = input('Please enter the dictionary\n-> ')
 translations_list = translations.split(', ')
 for translations in translations_list:
     list_of_values = translations.split(':')
     key = list_of_values[0]
     value = list_of_values[1]
     dictionary[key] = value
-print(dictionary)
+
+values_list = dictionary.values()
+sentence = input('Please enter a sentence\n-> ')
+sentence_list = sentence.split(' ')
+print('The translation is ', end = '')
+for sentence in sentence_list:
+    if sentence in dictionary.keys():
+        print(dictionary[sentence], end=' ')
+    else:
+        print(sentence, end = ' ')
+
