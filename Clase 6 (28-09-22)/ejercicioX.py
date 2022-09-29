59f2a5d736542246480c810a4c88dbd394fe0453
mginez <mginez@correo.unimet.edu.ve>
2022-09-29 19:49:37 -0400

    🐍 Diccionarios

--- Clase 6 (28-09-22)/ejercicioX.py
+++ Clase 6 (28-09-22)/ejercicioX.py
@@ -1,9 +1,12 @@
 number = int(input('Please enter a number from 2 to 12: '))
-combo = 0
-count = 1
-for aux in range [1, number+1]:
-    aux += count 
-    if combo == number:
-        print (aux, combo)
-
-    
+acum=0
+count=0
+list = []
+print(f'The pairs of numbers in the dice that added are equal to number {number} are: ')
+for aux in range (1, number):
+    acum = number - aux
+    if aux and acum not in list:
+        list.append(aux)
+        list.append(acum)
+        print (list[count], 'and', list[count+1])
+    count += 2   
