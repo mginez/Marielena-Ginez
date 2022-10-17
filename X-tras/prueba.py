d1e11fc4ded37c9af393ff1abb5b3e5c2185cfc2
mginez <mginez@correo.unimet.edu.ve>
2022-10-17 16:55:29 -0400

    🦔 ya entendí funciones

---
+++ X-tras/prueba.py
@@ -0,0 +1,16 @@
+study_average = {
+        'U':{
+            'Clients': 10,
+            'Total': 210
+            },
+        'T':{
+            'Clients': 0,
+            'Total': 0  
+            },
+        'R':{
+            'Clients': 0,
+            'Total': 0
+        }
+        }
+
+print(study_average['U']['Total']/study_average['U']['Clients'])
