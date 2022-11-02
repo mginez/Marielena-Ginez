3f512c2a15202d06f0bd4d15a924b1314de35d80
mginez <mginez@correo.unimet.edu.ve>
2022-11-02 10:06:55 -0400

    🌵Ejercicio tipo Parcial 2

--- Clase 14 (26-10-22)/Ejercicio 2/Advertisement.py
+++ Clase 14 (26-10-22)/Ejercicio 2/Advertisement.py
@@ -1,3 +1,28 @@
-class Advertisement:
-    def __init__(self) -> None:
-        pass
+class ComAdvertisement: #Commercial Advertisement
+    def __init__(self, picture, section):
+        self.picture = picture
+        self.section = section
+
+class DeclAdvertisement: #Declassified Advertisement
+    def __init__(self, price, pub_date, days, type):
+        self.price = price
+        self.publication_date = pub_date
+        self.days = days
+        self.type = type
+
+class DeclAdvertisementVeh(DeclAdvertisement): #Declassified Advertisement Vehicle
+    def __init__(self, price, pub_date, days, type, brand, model, year, color, km):
+        super().__init__(self, price, pub_date, days, type)
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+        self.km = km
+
+class DeclAdvertisementViv(DeclAdvertisement): #Declassified Advertisement Vivienda
+    def __init__(self, price, pub_date, days, type, m2, rooms, bathrooms):
+        super().__init__(self, price, pub_date, days, type)
+        self.m2 = m2
+        self.rooms = rooms
+        self.bathrooms = bathrooms
+
---
+++ Clase 14 (26-10-22)/Ejercicio 2/Section.py
@@ -0,0 +1,4 @@
+class Section:
+    def __init__(self, head, name):
+        self.head_writters = head #!!!
+        self.name = name
